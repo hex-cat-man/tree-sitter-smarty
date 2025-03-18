5670edc7d11ccc83946a1d9d9916918da9b79664
hex-cat-man <hex-cat-man@tutanota.com>
2025-03-18 18:56:44 +0100

    feat: tags and blocks
    General syntax of tags, blocks, comments and text.

--- grammar.js
+++ grammar.js
@@ -7,11 +7,82 @@
 /// <reference types="tree-sitter-cli/dsl" />
 // @ts-check
 
+// This is just my assumtion what a valid smarty identifier might look like.
+//
+// https://www.php.net/manual/en/language.variables.basics.php
+const REGEX_IDENTIFIER = /[a-zA-Z_\x80-\xff][a-zA-Z0-9_\x80-\xff]*/;
+
 module.exports = grammar({
   name: "smarty",
 
+  conflicts: $ => [
+    [$._smarty, $.block],
+  ],
+
+  extras: $ => [
+    $.comment,
+    /\s/,
+  ],
+
   rules: {
-    // TODO: add the actual grammar rules
-    source_file: $ => "hello"
+    template: $ => repeat($._smarty),
+    _smarty: $ => choice(
+      $.tag,
+      $.block,
+      $.text,
+    ),
+
+    // Tag delimiters followed by whitespace are treated as text.
+    text: _ => prec.right(repeat1(
+      choice(
+        /[^\{]+|\{/,
+        // Docs say "surrounded by whitespace" but the only requirement seems
+        // to be that the left delimiter is followed by whitespace.
+        token.immediate(seq('{', /\s/)),
+      ),
+    )),
+
+    // TODO: I would like to match the '{' and '*' as separate tokens to be able
+    //       to highlight them differently (like in the smarty documentation).
+    //       But we cannot use token.immidiate (I think?), since comments
+    //       themselfs are extras.
+    comment: _ => seq(
+      '{*',
+      repeat(choice(
+        /[^*]+/,
+        token.immediate(seq('*', /[^\}]/)),
+      )),
+      '*}',
+    ),
+
+    identifier: _ => REGEX_IDENTIFIER,
+
+    tag: $ => seq(
+      '{',
+      $.identifier,
+      '}',
+    ),
+
+    end_tag: $ => seq(
+      '{/',
+      $.identifier,
+      '}',
+    ),
+
+    block: $ => seq(
+      $.tag,
+      alias(repeat($._smarty), $.body),
+      $.end_tag,
+    ),
+
+    // TODO: tag_function and tag_attributes.
+    //
+    // tag_function: $ => seq(
+    //   $.tag_function_name,
+    //   $.tag_attributes,
+    // ),
+    // tag_function_name: $ => alias($.identifier, 'tag_function_name'),
+    // tag_attributes: $ => repeat($.tag_attribute),
+    // tag_attribute: $ => choice(/* ... */),
   }
 });
---
+++ queries/highlights.scm
@@ -0,0 +1,6 @@
+[ "{" "}" "{/" ] @punctuation.bracket
+
+(tag (identifier) @tag)
+(end_tag (identifier) @tag)
+
+(comment) @comment
