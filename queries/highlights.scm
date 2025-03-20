27d477c502790051bb4a64fd5fe19c66049a53c8
hex-cat-man <hex-cat-man@tutanota.com>
2025-03-20 19:59:24 +0100

    fix(highlights): add missing "!" token

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -1,8 +1,20 @@
-["{" "}" "{/"] @punctuation.bracket
+[
+  "{"
+  "}"
+  "{/"
+] @punctuation.bracket
 
-["[" "]" "(" ")"] @punctuation.bracket
+[
+  "["
+  "]"
+  "("
+  ")"
+] @punctuation.bracket
 
-["," ":"] @punctuation.delimiter
+[
+  ","
+  ":"
+] @punctuation.delimiter
 
 [
   "="
@@ -10,6 +22,7 @@
   "->"
   "."
   "|"
+  "!"
 
   "+"
   "-"
